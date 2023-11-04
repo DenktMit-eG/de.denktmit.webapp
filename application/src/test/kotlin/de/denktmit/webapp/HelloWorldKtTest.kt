da8ad4b477a966417d0e9a847028def6d0cf8983
Marius Schmidt <marius@denktmit.de>
2023-11-04 18:55:36 +0100

    Added Jacoco report generation for whole project

--- application/src/test/kotlin/de/denktmit/webapp/HelloWorldKtTest.kt
+++ application/src/test/kotlin/de/denktmit/webapp/HelloWorldKtTest.kt
@@ -3,8 +3,6 @@ package de.denktmit.webapp
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 class HelloWorldKtTest {
 
     @Test
