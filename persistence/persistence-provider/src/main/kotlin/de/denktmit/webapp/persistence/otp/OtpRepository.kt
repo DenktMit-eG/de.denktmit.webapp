f17f47988808af3eceb531ada7d12797f9a745af
Marius Schmidt <marius@denktmit.de>
2023-11-04 22:39:52 +0100

    Reduced warnings

--- persistence/persistence-provider/src/main/kotlin/de/denktmit/webapp/persistence/otp/OtpRepository.kt
+++ persistence/persistence-provider/src/main/kotlin/de/denktmit/webapp/persistence/otp/OtpRepository.kt
@@ -3,7 +3,4 @@ package de.denktmit.webapp.persistence.otp
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
-interface OtpRepository: CrudRepository<OtpAction, UUID> {
-
-
-}
+interface OtpRepository: CrudRepository<OtpAction, UUID>
--- persistence/persistence-provider/src/main/kotlin/de/denktmit/webapp/persistence/users/UserRepository.kt
+++ persistence/persistence-provider/src/main/kotlin/de/denktmit/webapp/persistence/users/UserRepository.kt
@@ -3,7 +3,4 @@ package de.denktmit.webapp.persistence.users
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
-interface UserRepository: CrudRepository<User, UUID> {
-
-
-}
+interface UserRepository: CrudRepository<User, UUID>
